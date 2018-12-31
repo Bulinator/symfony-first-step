2a2368279ed097d8873379dc1cb62319ef6fd7ec
bulotge <bulotge@gisis.eu>
2018-12-31 18:29:20 +0100

    feat: CRUD

---
+++ src/Controller/admin/PropertyController.php
@@ -0,0 +1,92 @@
+<?php
+namespace App\Controller\admin;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\PropertyType;
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+
+
+class PropertyController extends AbstractController
+{
+    public function __construct(PropertyRepository $repository, ObjectManager $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/admin", name="admin.property.index")
+     */
+    public function index()
+    {
+        $properties = $this->repository->findAll();
+        return $this->render('admin/property/index.html.twig', compact('properties'));
+    }
+
+    /**
+     * @Route("/admin/property/create", name="admin.property.new")
+     */
+    public function new(Request $request)
+    {
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($property); // persist entity before to save db
+            $this->em->flush();
+            $this->addFlash('success', 'The item has been created');
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+        return $this->render('admin/property/new.html.twig', [
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/property/{id}", name="admin.property.edit", methods="GET|POST")
+     * @param Property
+     */
+    public function edit(Property $property, Request $request)
+    {
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->flush();
+            $this->addFlash('success', 'The item has been updated');
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+        return $this->render('admin/property/edit.html.twig', [
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("admin/property/delete/{id}", name="admin.property.delete", methods="DELETE")
+     */
+    public function delete(Property $property, Request $request)
+    {
+        // could be use by method but for tuto is enough ;)
+        // not working, looks like CSRF token is not valid....?!!
+        if ($this->isCsrfTokenValid('delete' . $property->getId(), $request->get('_token'))) {
+            dump('SUPPRESSION');
+            //$this->em->remove($property);
+            //$this->em->flush();
+            $this->addFlash('success', 'The item has been deleted');
+            return new Response('Suppression');
+        }
+
+        return $this->redirectToRoute('admin.property.index');
+    }
+
+}
